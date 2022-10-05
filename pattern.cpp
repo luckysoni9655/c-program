34fadada5c0a79e98b0b083fef28d61a87f526b2
luckysoni9655 <114732203+luckysoni9655@users.noreply.github.com>
2022-10-05 23:11:58 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<=3;j++)
+        {
+            cout<<"*"<<" ";
+        }cout<<endl;
+    }*/
+
+    /*int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(i==0 ||i==4)
+            {
+                cout<<"*"<<" ";
+            }
+            else if(j==0|| j==4)
+            {
+                cout<<"*"<<" ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }*/
+   /* int i,j,n;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<"*"<<" ";
+       
+        }
+        cout<<endl;
+    }*/
+    /*int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j<=n-i)
+        {
+        cout<<"  ";        
+        }
+        else
+        {
+            cout<<"*"<<" ";
+        }
+        }
+        cout<<endl;
+    }*/
+   /* int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+          
+     
+            cout<<i;
+        
+           } cout<<endl;
+        
+        }
+*/
+/*int i,j,n,count=1;
+cin>>n;
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        cout<<count<<" ";
+        count++;
+    }cout<<endl;
+}*/    
+   
+    return(0);
+}
